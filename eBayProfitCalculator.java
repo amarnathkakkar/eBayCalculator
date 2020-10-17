9184a0ffba4bfd504c636aa349c5c060d72f4f43
amarnathkakkar <ak2022@bath.ac.uk>
2020-10-17 15:18:27 +0100

    Added variable for ebay percentage fee. Fixed giveGovVAT.

--- eBayProfitCalculator.java
+++ eBayProfitCalculator.java
@@ -16,14 +16,6 @@ public class eBayProfitCalculator{
 	Scanner usrInput;
 
 
-	public eBayProfitCalculator() {
-		flatPaypalFee = 0.3;
-		percEbayFee = 0.09*(1+vat);
-		percPaypalFee = 0.029;
-
-		usrInput = new Scanner(System.in);
-	}
-
 	public eBayProfitCalculator(double itemCostUser, double shippingCostUser, double parcelCostUser, InputStream testInput) {
 		itemCost = itemCostUser;
 		shippingCost = shippingCostUser;
@@ -39,31 +31,38 @@ public class eBayProfitCalculator{
 	}
 
 
+
+	public eBayProfitCalculator() {
+		flatPaypalFee = 0.3;
+		percPaypalFee = 0.029;
+
+		usrInput = new Scanner(System.in);
+	}
+
+
 	public static void main(String args[]) {
+
 		eBayProfitCalculator newCalc = new eBayProfitCalculator();
-		if(newCalc.setVariablesFromFile()) {
-			newCalc.printVariables();
-			System.out.println("Type anything other than a number to exit.");
-			newCalc.Start();
-		}
+
+		newCalc.setVariablesFromFile();
+		newCalc.printVariables();
+		System.out.println("Type anything other than a number to exit.");
+		newCalc.Start();
+		
 	}
 
-	public void printVariables() {
-		System.out.println("itemCost = " + itemCost + " shippingCost = " + shippingCost + " parcelCost = " + parcelCost + " VAT = " + vat*100 + "%");
-	}
 
-	public boolean setVariablesFromFile() {
-		boolean variableSet = false;
+	public void setVariablesFromFile() {
 
 		try {
-			File varFile = new File("Variables.txt");
+			File varFile = new File("Variables.txt");	
 			Scanner fileScanner = new Scanner(varFile);
 
 			int i = 0;
-			String[] variableArray = new String[4];
+			String[] variableArray = new String[5];
 
 			while(fileScanner.hasNextLine()) {
-				variableArray[i] = fileScanner.nextLine();
+				variableArray[i] = fileScanner.nextLine().replaceAll("[^(\\d+)\\.(\\d+)]",""); //use split string into array
 				i++;
 			}
 			fileScanner.close();
@@ -72,128 +71,37 @@ public class eBayProfitCalculator{
 			shippingCost = Double.parseDouble(variableArray[1]);
 			parcelCost = Double.parseDouble(variableArray[2]);
 			vat = Double.parseDouble(variableArray[3]);
+			percEbayFee = (1+vat)*Double.parseDouble(variableArray[4]);
 
-			variableSet = true;
 
 		} catch (FileNotFoundException e) {
 			System.out.println("File named 'Variables.txt' not found.\n");
 			e.printStackTrace();
+			System.exit(1);
 		} catch (NumberFormatException e) {
 			System.out.println("Please ensure variables in file 'Variables.txt' are real numbers.\n");
 			e.printStackTrace();
+			System.exit(1);
 		}
 		
-		return variableSet;
-	}
-
-
-	public double Round(double input) {
-		return Double.parseDouble(String.format("%.2f",input));
-	}
-
-
-	public void Exit() {
-		try {
-			System.out.print("exiting");
-            Thread.sleep(200);
-            System.out.print(".");
-            Thread.sleep(200);
-            System.out.print(".");
-            Thread.sleep(200);
-            System.out.print(".\n");
-            Thread.sleep(200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-	}
-
-
-	public ArrayList<Integer> populateNumOfGoodsToCheckArray(Scanner usrInput) {
-		ArrayList<Integer> intArray = new ArrayList<Integer>();
-
-		do {
-			//System.out.println("Leave empty or type an integer greater than 0.");
-			System.out.print("numberOfGoods = ");
-			String numberOfGoodsString = usrInput.nextLine();
-
-			if (isValidDouble(numberOfGoodsString)) {
-				intArray.add(Integer.parseInt(numberOfGoodsString));
-			} else {
-				break;
-			}
-		} while (true);
-
-		
-		if (intArray.size() < 1) {
-			for (int k=1; k<=10; k++) {
-				intArray.add(k);
-			}
-		}
-
-		return intArray;
-	}
-
-
-	public void printSellingAndProfit(int n, double sellingPrice, double profit) {
-		System.out.println(n + ":  s = " + sellingPrice + ", p = " + profit);
-	}
-
-	
-	public void Loop(boolean bool) {
-		if (bool == true) {
-			Start();
-		} else {
-			Exit();
-		}
-	}
-	
-
-	public boolean isValidDouble(String input) {
-		boolean isValid = false;
-
-		if (input.matches("-?\\d+(\\.\\d+)?")) {
-			isValid = true;
-		}
-
-		return isValid;
-	}
-
-
-	public double[] calcSellingPriceAndProfit(int n, double priceOfOneGood) {
-		//sell price of goods = price of a single good * number of goods sold
-		double sellingPrice = Round(priceOfOneGood*n);
-		//cost of goods sold = cost of goods sold + shippingCost + parcelCost + flat paypal fee + %(ebayfee & paypalfee) of selling price
-		double costOfGoods = Round(itemCost*n + (shippingCost + parcelCost + flatPaypalFee) + (percPaypalFee+percEbayFee)*sellingPrice);
-
-		double claimBackVAT = Round(vat*(itemCost*n + percEbayFee*sellingPrice));
-		//if claiming VAT from shipping, use below 'claimBackVAT' (probably once I have got a business royal mail acc? Since I can view reports)
-		//double claimBackVAT = round(vat*(itemCost*n + shippingCost + percEbayFee*sellingPrice));
-		double giveGovVAT = Round(vat*sellingPrice);
-
-		double profit = Math.min(
-				Round(sellingPrice+claimBackVAT-costOfGoods-giveGovVAT), Round(sellingPrice-costOfGoods)
-		);
-
-
-		double[] doubleArray = new double[2];
-		doubleArray[0] = sellingPrice;
-		doubleArray[1] = profit;
-
-
-		return doubleArray;
+		//return variableSet;
 	}
 
 
 	public void Start() {
-
-		double priceOfOneGood = 0;
 		
 		System.out.print("priceOfOneGood = ");
-
-
 		String priceOfOneGoodString = usrInput.nextLine();
+		boolean isValidInput = true;
 
-		if (isValidDouble(priceOfOneGoodString)) {
+
+		double priceOfOneGood = 0;
+
+
+		isValidInput = isValidDouble(priceOfOneGoodString);
+
+		
+		if(isValidInput) {
 			priceOfOneGood = Double.parseDouble(priceOfOneGoodString);
 
 
@@ -239,10 +147,116 @@ public class eBayProfitCalculator{
 				});
 
 			}
-
 		}
-		Loop(isValidDouble(priceOfOneGoodString));
 
+		Loop(isValidInput);
 	}
 
+
+	public ArrayList<Integer> populateNumOfGoodsToCheckArray(Scanner usrInput) {
+		ArrayList<Integer> intArray = new ArrayList<Integer>();
+
+		do {
+			//System.out.println("Leave empty or type an integer greater than 0.");
+			System.out.print("numberOfGoods = ");
+			String numberOfGoodsString = usrInput.nextLine();
+
+			if (isValidDouble(numberOfGoodsString)) {
+				intArray.add(Integer.parseInt(numberOfGoodsString));
+			} else {
+				break;
+			}
+		} while (true);
+
+		
+		if (intArray.size() < 1) {
+			for (int k=1; k<=10; k++) {
+				intArray.add(k);
+			}
+		}
+
+		return intArray;
+	}
+
+
+	public double[] calcSellingPriceAndProfit(int n, double priceOfOneGood) {
+
+		double sellingPrice, costOfGoods, claimBackVAT, giveGovVAT, profit;
+
+		//sell price of goods = price of a single good * number of goods sold
+		sellingPrice = Round(priceOfOneGood*n);
+
+		//cost of goods sold = cost of goods sold + shippingCost + parcelCost + flat paypal fee + %(ebayfee & paypalfee) of selling price
+		costOfGoods = Round(itemCost*n + (shippingCost + parcelCost + flatPaypalFee) + (percPaypalFee+percEbayFee)*sellingPrice);
+
+		//if claiming VAT from shipping, use below 'claimBackVAT' (probably once I have got a business royal mail acc? Since I can view reports)
+		//double claimBackVAT = round(vat*(itemCost*n + shippingCost + percEbayFee*sellingPrice));
+		claimBackVAT = Round(vat*(itemCost*n + percEbayFee*sellingPrice));
+		
+
+		giveGovVAT = Round(sellingPrice*(vat/(1+vat)));
+
+		profit = Math.min(
+				Round(sellingPrice+claimBackVAT-costOfGoods-giveGovVAT), Round(sellingPrice-costOfGoods)
+		);
+
+
+		double[] doubleArray = new double[2];
+		doubleArray[0] = sellingPrice;
+		doubleArray[1] = profit;
+
+
+		return doubleArray;
+	}
+
+
+	public void printSellingAndProfit(int n, double sellingPrice, double profit) {
+		System.out.println(n + ":  s = " + sellingPrice + ", p = " + profit);
+	}
+
+
+	public void Loop(boolean bool) {
+		if (bool == true) {
+			Start();
+		} else {
+			quitApp();
+		}
+	}
+	
+
+	public boolean isValidDouble(String input) {
+		boolean isValid = false;
+
+		if (input.matches("-?\\d+(\\.\\d+)?")) {
+			isValid = true;
+		}
+
+		return isValid;
+	}
+
+
+	public void printVariables() {
+		System.out.println("itemCost = " + itemCost + " shippingCost = " + shippingCost + " parcelCost = " + parcelCost + " VAT = " + vat*100 + "%");
+	}
+
+
+	public double Round(double input) {
+		return Double.parseDouble(String.format("%.2f",input));
+	}
+
+
+	public void quitApp() {
+		try {
+			System.out.print("exiting");
+            Thread.sleep(200);
+            System.out.print(".");
+            Thread.sleep(200);
+            System.out.print(".");
+            Thread.sleep(200);
+            System.out.print(".\n");
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+	}
 }
